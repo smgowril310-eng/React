92b121a11121ac8ab6f884bc8e488feb95eda7da
Gowri <smgowril310@gmail.com>
2026-07-23 20:49:46 -0400

    Frontend committed

---
+++ ems-frontend/src/Component/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function FooterComponent() {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>All rights reserved by code logic - Tamil(2024)</span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
