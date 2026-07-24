9502c3998b856ba2850c0cd1a90e609110e27169
Gowri <smgowril310@gmail.com>
2026-07-24 18:25:00 -0400

    last commit

--- ems-frontend/src/Services/EmployeeService.js
+++ ems-frontend/src/Services/EmployeeService.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const REST_API_BASE_URL = 'http://localhost:8080/api/employees';
+const REST_API_BASE_URL = 'https://ems-project-production-2623.up.railway.app/api/employees';
 
 export const listEmployees = () => axios.get(REST_API_BASE_URL);
 
